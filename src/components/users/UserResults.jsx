041500dd1ed2ad266a8a6857b871d371af6f7fc4
Bidipto Roy <bidiptoroy@gmail.com>
2022-07-07 12:52:46 +0530

    Adding all files

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,29 @@
+import {  useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
+
+function UserResults() {
+  const { users, loading } = useContext(GithubContext);
+
+  // useEffect(() => {
+  // fetchUsers
+  // }, []);
+
+  if (!loading) {
+    return (
+      <div
+        className="grid grid-cols-1 gap-8 
+    xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2"
+      >
+        {users.map((it) => (
+          <UserItem key={it.id} user={it} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
